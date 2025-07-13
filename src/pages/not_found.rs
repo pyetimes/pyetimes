591a023bfdee41b769c8beb317b062e18b6c3974
Daril Rodriguez <darilrodriguez.2@gmail.com>
2025-07-13 14:29:42 -0300

    fix 404 not found page

--- src/pages/not_found.rs
+++ src/pages/not_found.rs
@@ -2,6 +2,7 @@ pub fn not_found() -> crate::pages::Page {
     let mut layout = magik::get("./pages/reader/layout.html");
     let not_found = magik::get("./pages/404.html");
 
+    layout.set("main_story", &());
     layout.set("body", &not_found);
 
     layout.into()
