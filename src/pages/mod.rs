034ae4e92afb00b923f1b6968fcee8f5fa6b08f4
Daril Rodriguez <darilrodriguez.2@gmail.com>
2025-07-31 17:06:14 -0300

    in process of add cache

--- src/pages/mod.rs
+++ src/pages/mod.rs
@@ -1,19 +1,54 @@
+use axum::Extension;
 use axum::extract::{Path, Query};
 use axum::http::StatusCode;
 use axum::response::Html;
 use axum::{Router, extract::State, routing::get};
+use axum_response_cache::CacheLayer;
 use magik::Renderable;
+use tower::Layer;
 
 use crate::models::ErrorPayload;
 use crate::repo::{ArticlesRepo, AuthorsRepo, FeedRepo};
 use crate::state::AppState;
 use crate::web::pages;
 
+#[derive(Clone, Debug)]
+pub struct CacheControlLayer;
+
+impl<S> Layer<S> for CacheControlLayer {
+    type Service = CacheControlLayerMiddleware<S>;
+
+    fn layer(&self, inner: S) -> Self::Service {
+        CacheControlLayerMiddleware { inner }
+    }
+}
+
+struct CacheControlLayerMiddleware<S> {
+    inner: S,
+}
+
+#[derive(Clone, Debug)]
+struct ArticleType {
+    pub is_draft: bool,
+}
+
 pub fn routes() -> Router<AppState> {
     Router::new()
-        .route("/", get(index))
-        .route("/editor", get(editor))
-        .route("/articles/{slug}", get(article_page))
+        .route("/", get(index).layer(CacheLayer::with_lifespan(3600)))
+        .route(
+            "/editor",
+            get(editor).layer(CacheLayer::with_lifespan(3600)),
+        )
+        .route(
+            "/articles/{slug}",
+            get(article_page)
+                .layer(Extension(ArticleType { is_draft: false }))
+                .layer(CacheLayer::with_lifespan(3600)),
+        )
+        .route(
+            "/drafts/{slug}",
+            get(article_page).layer(Extension(ArticleType { is_draft: true })),
+        )
         .route("/register", get(register))
 }
 
@@ -74,6 +109,7 @@ async fn editor(State(state): State<AppState>, id: Query<GetParams>) -> Html<Str
 async fn article_page(
     State(state): State<AppState>,
     Path(slug): Path<String>,
+    Extension(article_type): Extension<ArticleType>,
 ) -> Result<Html<String>, ErrorPayload> {
     let article = ArticlesRepo::get_by_slug(&state.db, &slug).await;
 
@@ -91,6 +127,14 @@ async fn article_page(
     }
 
     let article = article.unwrap();
+
+    if article_type.is_draft {
+        if article.published {
+            return Ok(Html(pages::NotFound {}.render()));
+        }
+    } else if !article.published {
+        return Ok(Html(pages::NotFound {}.render()));
+    }
 
     let author = AuthorsRepo::get_by_id(&state.db, article.author_id).await;
 
