da20eb979b16096032f09a534e39754fbae53351
Daril Rodriguez <darilrodriguez.2@gmail.com>
2025-08-06 00:48:42 -0300

    Improve style and responsive

--- web/components/header.mk
+++ web/components/header.mk
@@ -1,19 +1,18 @@
 <header class="header">
-    <div class="header-title">
+    <div class="header-info">
         <x-time></x-time>
+        <span>Dias sin Bugs: 0</span>
+    </div>
+    <div class="header-title">
         <div>
             <h1 class="masthead"><a href="/">PyE TIMES</a></h1>
             <p class="tagline">"Todas las Noticias que Merecen Compilar"</p>
         </div>
-        <span>Dias sin Bugs: 0</span>
     </div>
     <nav class="navbar">
         <ul class="nav-links">
-            <li><a href="#">Chismes</a></li>
-            <li><a href="#">Pie</a></li>
-            <li><a href="#">York</a></li>
-            <li><a href="#">Times</a></li>
-            <li><a href="#">Mas chismes en pie no hay programacion</a></li>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Sobre Nosotros</a></li>
         </ul>
     </nav>
 </header>
