a41bf49b85227b7fcd05dc41f51c8ef53d8dbb6e
Daril Rodriguez <darilrodriguez.2@gmail.com>
2025-08-07 00:25:51 -0300

    Improve responsive style of navbar and add dark mode (inverted)

--- web/components/header.mk
+++ web/components/header.mk
@@ -1,18 +1,40 @@
 <header class="header">
-    <div class="header-info">
-        <x-time></x-time>
-        <span>Dias sin Bugs: 0</span>
-    </div>
     <div class="header-title">
         <div>
             <h1 class="masthead"><a href="/">PyE TIMES</a></h1>
             <p class="tagline">"Todas las Noticias que Merecen Compilar"</p>
         </div>
     </div>
-    <nav class="navbar">
+    <div class="header-info">
+        <div>
+            <x-time></x-time>
+            <br />
+            <span>Dias sin Bugs: 0</span>
+        </div>
+        <button class="menu" onclick="toggleNav()"></button>
+        <button class="theme-toggle" onclick="toggleTheme()"></button>
+    </div>
+    <nav id="navbar" class="navbar">
         <ul class="nav-links">
             <li><a href="/">Inicio</a></li>
             <li><a href="/about">Sobre Nosotros</a></li>
         </ul>
     </nav>
 </header>
+<script>
+    function toggleNav() {
+        const navbar = document.getElementById('navbar');
+        navbar.classList.toggle('active');
+    }
+
+    function toggleTheme() {
+        document.documentElement.classList.toggle('dark-theme');
+        window.localStorage.setItem('theme', document.documentElement.classList.contains('dark-theme') ? 'dark' : 'light');
+    }
+
+    window.onload = function() {
+        if (window.localStorage.getItem('theme') === 'dark') {
+            document.documentElement.classList.add('dark-theme');
+        }
+    };
+</script>
